7a5d1a59a724e3c80bcfd398db41349e53cf552e
saad56hussain-lab <saad56hussain@gmail.com>
2026-02-02 22:36:45 +0500

    update

---
+++ assets/main.js
@@ -0,0 +1,76 @@
+document.getElementById("navbar").innerHTML = `
+<nav class="navbar">
+    <div class="logo">NAIL ART</div>
+
+    <ul class="nav-links">
+        <li><a href="index.html">Home</a></li>
+
+        <li><a href="about.html">About</a></li>
+
+        <li class="dropdown">
+            <a href="#">Categories ▾</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Kids (Easy)</a></li>
+                <li><a href="#">Teens / Adults</a></li>
+                <li><a href="#">Complex Designs</a></li>
+            </ul>
+        </li>
+
+        <li class="dropdown">
+            <a href="#">Designs ▾</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Floral</a></li>
+                <li><a href="#">Geometric</a></li>
+                <li><a href="#">Faces</a></li>
+                <li><a href="#">Abstract</a></li>
+                <li><a href="#">Festive</a></li>
+            </ul>
+        </li>
+
+        <li><a href="#">Tutorials</a></li>
+        <li><a href="contact.html">Contact</a></li>
+    </ul>
+</nav>`
+document.getElementById("footer").innerHTML = `
+<footer class="footer">
+<div class="child2">
+<h2>NAIL ART</h2>
+<p>Nail Art is a user-friendly website designed to inspire creativity by providing nail art designs, categories, and step-by-step tutorials for all age groups.</p>
+</div>
+<div class="child3">
+    <h2>HELPFUL LINKS</h2>
+<a href="index.html">HOME</a>
+<a href="about.html">ABOUT</a>
+<a href="">CATEGORIES</a>
+<a href="">DESIGNS</a>
+<a href="">TUTORIALS</a>
+<a href="">CONTACT</a>
+</div>
+<div class="child4">
+    <h2>CATEGORIES</h2>
+<a href="">KIDS</a>
+<a href="">TEEN/ADULTS</a>
+<a href="">COMPLEX DESIGNS</a>
+</div>
+<div class="child1">
+    <h2>DESIGNS</h2>
+<a href="">FLORAL</a>
+<a href="">GEOMATRIC</a>
+<a href="">FACES</a>
+<a href="">ABSTRACT</a>
+<a href="">FESTIVE</a>
+</div>
+<div class="child5">
+    <h2>CONTACT</h2>
+<a href="">
+    <i class="fa-solid fa-location-dot"></i>
+    Aptech Metro Star Gate Karachi.</a>
+<a href="">
+    <i class="fa-solid fa-phone"></i>
+    +1-28272300*, *2466</a>
+<a href="">
+    <i class="fa-solid fa-envelope"></i> iayansyed2008@gmail.com
+</a>
+</div>
+    </footer>
+`
